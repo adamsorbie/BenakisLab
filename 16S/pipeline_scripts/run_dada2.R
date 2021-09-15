e8029045e784f269e377fbd02d35a562fe10619f
adamsorbie <adam.sorbie@med.uni-muenchen.de>
2021-09-15 14:51:03 +0200

    added windows support to run_dada2 script and function to automatically install packages

--- 16S/pipeline_scripts/run_dada2.R
+++ 16S/pipeline_scripts/run_dada2.R
@@ -13,15 +13,13 @@
 #' enhance taxonomic classification if possible 
 #' add third table with best hit formatting 
 #' remove any redundant code 
-#' function to install missing packages 
+#' better error handling 
 
 ### LIBRARIES 
-library(dada2)
-library(Biostrings)
-library(optparse)
-library(ggpubr)
-library(stringr)
-library(tictoc)
+if(!require("pacman")){
+  install.packages("pacman", repos = "http://cran.us.r-project.org")
+}
+pacman::p_load(dada2, Biostrings, optparse, ggpubr, stringr, tictoc)
 
 ### CMD OPTIONS
 
@@ -43,7 +41,7 @@ option_list <- list(
   make_option(c("-N", "--n_errorsR"),  type="integer", default =2,
               help=" expected errors for reverse reads",
               action = "store"),
-  make_option(c("-t", "--threads"),  type="integer", default=detectCores(),
+  make_option(c("-t", "--threads"),  type="integer", default=2,
               help="Number of threads",
               action = "store")
 )
@@ -51,21 +49,31 @@ option_list <- list(
 opt_parser <- OptionParser(option_list=option_list)
 opt <- parse_args(opt_parser)
 
+# check path exists 
 if (is.null(opt$path)) {
   print_help(opt_parser)
   stop("At least one argument must be supplied (path)", call.=FALSE)
 }
 
+# check selected number of threads is ok
+if (opt$threads > detectCores()){
+  stop("Selected number of threads exceeds available, exiting", call. = FALSE)
+}
+
 ### FUNCTIONS
+
+# convert to relative abundance
 normalize <- function(asv_tab) {
     rel_tab <- t(100 * t(asv_tab) / colSums(asv_tab))
     return(as.data.frame(rel_tab))
 }
 
+# strip n chars from right 
 RIGHT <- function(x,n){
   substring(x,nchar(x)-n+1)
 }
 
+# filter abundance using relative abundance cutoff 
 filter_abundance <- function(asv_tab) {
   # keeps ASVs which are above 0.25% rel abund in at least one sample 
   rel <- normalize(asv_tab)
@@ -73,6 +81,10 @@ filter_abundance <- function(asv_tab) {
   idx <- rownames(rel_filt)
   asv_tab_out <- asv_tab[idx, ]
   return(asv_tab_out)
+}
+
+checkOS <- function(){
+   return(.Platform$OS.type)
 }
 
 ### MAIN
@@ -84,9 +96,12 @@ if (RIGHT(opt$path, 1) != "/") {
 if (RIGHT(opt$out, 1) != "/") {
   opt$out <- paste0(opt$out, "/")
 }
+
+# time analysis
 tic()
 print(paste("ANALYSIS STARTING", Sys.time(), sep=" "))
 
+# set cmd params as variables 
 path <- opt$path 
 setwd(path)
 
@@ -95,22 +110,22 @@ dir.create(opt$out)
 maxE <- c(opt$n_errorsF, opt$n_errorsR)
 trunc_params <- c(opt$trunc_f, opt$trunc_r)
 
-# check reads follow illumina naming format here and exit if not
-# for now this will work but needs to be expanded - manually checking before 
-# should reduce need for this but still better to have it fully working
-illegal_chars <- c("-","#")
+# check for illegal characters in fastq filenames and exit if detected 
+illegal_chars <- c("-","#", "@", " ")
 check_fnames <- str_detect(list.files(pattern = "*.fastq.gz"), 
                            illegal_chars)
+
 if (any(check_fnames == TRUE)){
-  print("filenames not in illumina format, exiting")
+  print("filenames not in illumina format see: https://support.illumina.com/help/BaseSpace_OLH_009008/Content/Source/Informatics/BS/NamingConvention_FASTQ-files-swBS.htm, 
+        exiting")
   stop()
 } else {
   print("filenames ok!")
 }
 
-# forward reads
+# forward read paths
 fnFs <- sort(list.files(pattern="R1_001.fastq.gz", full.names = TRUE))
-# reverse reads
+# reverse read paths
 fnRs <- sort(list.files(pattern="R2_001.fastq.gz", full.names = TRUE))
 
 # get sample names from forward reads 
@@ -127,13 +142,15 @@ filtFs <- file.path("filtered", paste0(sample.names, "_filt_R1_001.fastq.gz"))
 # output path for filtered R reads
 filtRs <- file.path("filtered", paste0(sample.names, "_filt_R2_001.fastq.gz"))
 
-
+# filter and trim reads 
 out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, maxEE=maxE, 
                      truncLen=trunc_params, rm.phix=TRUE,
                      truncQ=3, compress=TRUE, multithread=opt$threads)
-# learn error rate 
+
+# learn error rate and plot error profiles 
 errF <- learnErrors(filtFs, multithread = opt$threads, nbases = 1e8, 
                     randomize = TRUE)
+
 jpeg(paste(opt$out, "error_plot_f.jpg", sep="/"))
 plotErrors(errF, nominalQ=TRUE)
 dev.off()
@@ -150,8 +167,12 @@ derepRs <- derepFastq(filtRs, verbose = TRUE)
 names(derepFs) <- sample.names
 names(derepRs) <- sample.names
 
-# remove filtered reads to reduce disk space
-system("rm -rf filtered || true")
+# remove filtered reads to reduce disk space 
+if (checkOS() == "unix"){
+  system("rm -rf filtered || true")
+} else if (checkOS() == "windows") {
+  system("if exists filtered rmdir /q /s filtered")
+}
 
 # run dada2 algorithm 
 dadaFs <- dada(derepFs, err=errF, multithread = opt$threads)
@@ -169,7 +190,8 @@ seq_length_distr <- table(nchar(getSequences(seqtab)))
 # filter reads by length
 seq_lengths <- as.numeric(names(seq_length_distr))
 
-# this is reasonable rule of thumb but  might be good to use quantiles in future
+# remove sequences which are longer or shorter than expected
+# 1.25 * mean length is a reasonable rule of thumb 
 MINLEN <- round(mean(seq_lengths) / 1.25, 0)  
 MAXLEN <- round(mean(seq_lengths) * 1.25, 0)
 seqlen <- nchar(getSequences(seqtab))
@@ -181,7 +203,7 @@ seqtab_chim_filt <- removeBimeraDenovo(seqtab.len_filt, method = "consensus",
 
 percent.nonchim <- sum(seqtab_chim_filt)/sum(seqtab)
 
-# pre-filter by 0.25% relative abundance (Reitmeier et al 2020, Researchsquare)
+# pre-filter by 0.25% relative abundance (Reitmeier et al 2021, ISME communication)
 print(paste("pre-filtering dimensions:", dim(t(seqtab_chim_filt))[1], sep=" "))
 seqtab_chim_abun_filt <- filter_abundance(t(seqtab_chim_filt))
 print(paste("post-filtering dimensions:", dim(seqtab_chim_abun_filt)[1], sep=" ")) 
@@ -189,6 +211,7 @@ seqtab_chim_abun_filt <- t(seqtab_chim_abun_filt)
 
 # check number of reads which made it through each step 
 getN <- function(x) sum(getUniques(x))
+# generate table summarizing steps 
 summary_tab <- data.frame(row.names=sample.names, dada2_input=out[,1],
                           filtered=out[,2], dada_f=sapply(dadaFs, getN),
                           dada_r=sapply(dadaRs, getN), merged=sapply(merged, getN),
@@ -200,6 +223,7 @@ summary_tab <- data.frame(row.names=sample.names, dada2_input=out[,1],
 qc_list <- list("Sequence_list_distribution" = seq_length_distr, 
                 "Stats" = summary_tab)
 
+# output sequence length distribution and study stats
 if (opt$int_quality_control == TRUE){
   
   seq_len_df <- data.frame(seq_length_distr)
@@ -212,6 +236,9 @@ if (opt$int_quality_control == TRUE){
   
 }
 
+## Taxonomic classification 
+
+# download silva files and assign taxonomy 
 
 if (!file.exists("silva_nr99_v138.1_train_set.fa.gz")){
   download.file(url="https://zenodo.org/record/4587955/files/silva_nr99_v138.1_train_set.fa.gz?download=1", 
@@ -226,16 +253,18 @@ if (!file.exists("silva_species_assignment_v138.1.fa.gz")){
                 destfile="silva_species_assignment_v138.1.fa.gz")
 }
 
+# addSpecies may improve % classified in some cases
 asv_taxa <- addSpecies(taxa, "silva_species_assignment_v138.1.fa.gz",
                        tryRC=TRUE)
 
-## write out files
+## write output files
 
 # create vector to contain headers
 asv_headers <- vector(dim(seqtab_chim_abun_filt) [2], mode = "character")
 # generate row and column names 
 rownames(asv_taxa) <- gsub(pattern=">", replacement="", x=asv_headers)
 
+# generate fasta headers
 for (i in 1:dim(seqtab_chim_abun_filt) [2]) {
   asv_headers[i] <- paste(">ASV", i, sep = "_")  
 }
@@ -244,7 +273,7 @@ asv_seqs <- colnames(seqtab_chim_abun_filt)
 asv_fasta <- c(rbind(asv_headers, asv_seqs))
 write(asv_fasta, paste(opt$out, "ASV_seqs.fasta", sep = "/"))
 
-# count table 
+# write feature count table 
 asv_tab <- t(seqtab_chim_abun_filt)
 row.names(asv_tab) <- sub(">", "", asv_headers)
 
@@ -252,7 +281,7 @@ write.table(asv_tab, paste(opt$out, "ASV_seqtab.tab", sep = "/"),
             sep = "\t", quote = F, col.names = NA)
 
 
-# tax table 
+# taxonomy table 
 row.names(asv_taxa) <- sub(">", "", asv_headers)
 asv_taxa <- as.data.frame(asv_taxa)
 asv_taxa$taxonomy <- paste(asv_taxa$Kingdom, asv_taxa$Phylum, asv_taxa$Class, 
@@ -261,7 +290,8 @@ asv_taxa$taxonomy <- paste(asv_taxa$Kingdom, asv_taxa$Phylum, asv_taxa$Class,
 
 # clean up taxa names
 asv_taxa$taxonomy <- gsub("NA","", asv_taxa$taxonomy)
-# write out asv tab with tax
+
+# write out feature count table with taxonomy included
 asv_tab_tax <- as.data.frame(asv_tab)
 asv_tab_tax$taxonomy <- paste(asv_taxa$taxonomy)
 write.table(asv_tab_tax, paste(opt$out, "ASV_seqtab_tax.tab", sep = "/"), 
@@ -270,10 +300,20 @@ write.table(asv_tab_tax, paste(opt$out, "ASV_seqtab_tax.tab", sep = "/"),
 # Phylogenetic tree construction 
 setwd(opt$out)
 
-system("muscle -in ASV_seqs.fasta -out aligned.fasta -maxiters 3")
-
-system("FastTree -quiet -nosupport -gtr -nt aligned.fasta > ASV_tree.tre")
-
+# align seqs with muscle and create tree with FastTree 
+if (checkOS() == "unix"){
+  system("muscle -in ASV_seqs.fasta -out aligned.fasta -maxiters 3")
+  
+  system("FastTree -quiet -nosupport -gtr -nt aligned.fasta > ASV_tree.tre")
+} else if (checkOS() == "windows"){
+  
+  #system("muscle.exe -in ASV_seqs.fasta -out aligned.fasta -maxiters 3")
+  
+  #system("FastTree.exe -quiet -nosupport -gtr -nt aligned.fasta > ASV_tree.tre")
+  
+  print("phylogenetic tree construction for windows is currently unsupported but
+        will be added in the near future")
+}
 print(paste("ANALYSIS COMPLETED", Sys.time(), sep=" "))
 
 # Send parameters to log file 
